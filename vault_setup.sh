8cd4a88963b617f7195d3f21e6eab8d135285475
Niteka Morel <n.morel@alustudent.com>
2026-02-19 15:42:15 +0200

    Delete vault_setup.sh

--- vault_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ~/secure_vault
-echo "Welcome to the Keys File" > ~/secure_vault/keys.txt
-echo "Welcome to the Secrets File" > ~/secure_vault/secrets.txt
-echo "Welcome to the Logs File" > ~/secure_vault/logs.txt
-echo "Vault Setup Complete!"
-ls -l ~/secure_vault
